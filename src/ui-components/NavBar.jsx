60074401b7e0da65936169c8b4b1f47cca0a746f
cdiaz2024 <cdiaz2024@communityschoolnaples.org>
2023-11-14 11:32:14 -0500

    image push

---
+++ src/ui-components/NavBar.jsx
@@ -0,0 +1,65 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "./utils";
+import { Flex, Text } from "@aws-amplify/ui-react";
+export default function NavBar(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="20px"
+      direction="row"
+      width="184px"
+      height="unset"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+      padding="24px 32px 24px 32px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...getOverrideProps(overrides, "NavBar")}
+      {...rest}
+    >
+      <Text
+        fontFamily="Inter"
+        fontSize="20px"
+        fontWeight="600"
+        color="rgba(0,0,0,1)"
+        textTransform="capitalize"
+        lineHeight="24.204544067382812px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        shrink="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="Logo"
+        {...getOverrideProps(overrides, "Logo")}
+      ></Text>
+      <Flex
+        gap="40px"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="space-between"
+        alignItems="center"
+        grow="1"
+        shrink="1"
+        basis="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 321")}
+      ></Flex>
+    </Flex>
+  );
+}
